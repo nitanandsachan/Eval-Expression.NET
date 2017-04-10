91a0c1fdd24a19cd3283bd7278a15ce2a84a9e60
nitanandsachan <sachan2019@hotmail.com>
2017-04-10 17:22:22 +0530

    Update Where.Designer.cs

--- src/Examples.Expressions.Eval/LINQ_Dynamic/Restriction_Operators/Where.Designer.cs
+++ src/Examples.Expressions.Eval/LINQ_Dynamic/Restriction_Operators/Where.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Examples.Expressions.Eval.LINQ_Dynamic.Restriction_Operators
+namespace Examples.Expressions.Eval.LINQ_Dynamic.Restriction_Operators
 {
     partial class Where
     {
@@ -38,9 +38,24 @@
             this.uiWhere_Simple_2_Execute = new System.Windows.Forms.Button();
             this.uiWhere_Simple_2_LINQ_Dynamic = new System.Windows.Forms.Button();
             this.uiWhere_Simple_2_LINQ = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.uiWhere_Simple_3_Execute = new System.Windows.Forms.Button();
+            this.uiWhere_Simple_3_LINQ_Dynamic = new System.Windows.Forms.Button();
+            this.uiWhere_Simple_3_LINQ = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.uiWhere_DrillDown_LINQ = new System.Windows.Forms.Button();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.uiWhere_Indexed_Execute_LINQ = new System.Windows.Forms.Button();
+            this.uiWhere_Indexed_Dynamic_LINQ = new System.Windows.Forms.Button();
+            this.uiWhere_Indexed_LINQ = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
@@ -145,11 +160,140 @@
             this.uiWhere_Simple_2_LINQ.UseVisualStyleBackColor = true;
             this.uiWhere_Simple_2_LINQ.Click += new System.EventHandler(this.uiWhere_Simple_2_LINQ_Click);
             // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.uiWhere_Simple_3_Execute);
+            this.groupBox4.Controls.Add(this.uiWhere_Simple_3_LINQ_Dynamic);
+            this.groupBox4.Controls.Add(this.uiWhere_Simple_3_LINQ);
+            this.groupBox4.Location = new System.Drawing.Point(276, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(120, 135);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Where - Simple 3";
+            // 
+            // uiWhere_Simple_3_Execute
+            // 
+            this.uiWhere_Simple_3_Execute.Location = new System.Drawing.Point(10, 100);
+            this.uiWhere_Simple_3_Execute.Name = "uiWhere_Simple_3_Execute";
+            this.uiWhere_Simple_3_Execute.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_Simple_3_Execute.TabIndex = 5;
+            this.uiWhere_Simple_3_Execute.Text = "Execute";
+            this.uiWhere_Simple_3_Execute.UseVisualStyleBackColor = true;
+            this.uiWhere_Simple_3_Execute.Click += new System.EventHandler(this.uiWhere_Simple_3_Execute_Click);
+            // 
+            // uiWhere_Simple_3_LINQ_Dynamic
+            // 
+            this.uiWhere_Simple_3_LINQ_Dynamic.Location = new System.Drawing.Point(10, 60);
+            this.uiWhere_Simple_3_LINQ_Dynamic.Name = "uiWhere_Simple_3_LINQ_Dynamic";
+            this.uiWhere_Simple_3_LINQ_Dynamic.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_Simple_3_LINQ_Dynamic.TabIndex = 4;
+            this.uiWhere_Simple_3_LINQ_Dynamic.Text = "LINQ Dynamic";
+            this.uiWhere_Simple_3_LINQ_Dynamic.UseVisualStyleBackColor = true;
+            this.uiWhere_Simple_3_LINQ_Dynamic.Click += new System.EventHandler(this.uiWhere_Simple_3_LINQ_Dynamic_Click);
+            // 
+            // uiWhere_Simple_3_LINQ
+            // 
+            this.uiWhere_Simple_3_LINQ.Location = new System.Drawing.Point(10, 20);
+            this.uiWhere_Simple_3_LINQ.Name = "uiWhere_Simple_3_LINQ";
+            this.uiWhere_Simple_3_LINQ.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_Simple_3_LINQ.TabIndex = 3;
+            this.uiWhere_Simple_3_LINQ.Text = "LINQ";
+            this.uiWhere_Simple_3_LINQ.UseVisualStyleBackColor = true;
+            this.uiWhere_Simple_3_LINQ.Click += new System.EventHandler(this.uiWhere_Simple_3_LINQ_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.button4);
+            this.groupBox5.Controls.Add(this.button5);
+            this.groupBox5.Controls.Add(this.uiWhere_DrillDown_LINQ);
+            this.groupBox5.Location = new System.Drawing.Point(402, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(120, 135);
+            this.groupBox5.TabIndex = 7;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Where - Drilldown";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 100);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Execute";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.uiWhere_DrillDown_Execute_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(10, 60);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "LINQ Dynamic";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.uiWhere_DrillDown_Dynamic_LINQ_Click);
+            // 
+            // uiWhere_DrillDown_LINQ
+            // 
+            this.uiWhere_DrillDown_LINQ.Location = new System.Drawing.Point(10, 20);
+            this.uiWhere_DrillDown_LINQ.Name = "uiWhere_DrillDown_LINQ";
+            this.uiWhere_DrillDown_LINQ.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_DrillDown_LINQ.TabIndex = 3;
+            this.uiWhere_DrillDown_LINQ.Text = "LINQ";
+            this.uiWhere_DrillDown_LINQ.UseVisualStyleBackColor = true;
+            this.uiWhere_DrillDown_LINQ.Click += new System.EventHandler(this.uiWhere_DrillDown_LINQ_Click);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.uiWhere_Indexed_Execute_LINQ);
+            this.groupBox6.Controls.Add(this.uiWhere_Indexed_Dynamic_LINQ);
+            this.groupBox6.Controls.Add(this.uiWhere_Indexed_LINQ);
+            this.groupBox6.Location = new System.Drawing.Point(528, 12);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(120, 135);
+            this.groupBox6.TabIndex = 7;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Where - Indexed";
+            // 
+            // uiWhere_Indexed_Execute_LINQ
+            // 
+            this.uiWhere_Indexed_Execute_LINQ.Location = new System.Drawing.Point(10, 100);
+            this.uiWhere_Indexed_Execute_LINQ.Name = "uiWhere_Indexed_Execute_LINQ";
+            this.uiWhere_Indexed_Execute_LINQ.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_Indexed_Execute_LINQ.TabIndex = 5;
+            this.uiWhere_Indexed_Execute_LINQ.Text = "Execute";
+            this.uiWhere_Indexed_Execute_LINQ.UseVisualStyleBackColor = true;
+            this.uiWhere_Indexed_Execute_LINQ.Click += new System.EventHandler(this.uiWhere_Indexed_Execute_LINQ_Click);
+            // 
+            // uiWhere_Indexed_Dynamic_LINQ
+            // 
+            this.uiWhere_Indexed_Dynamic_LINQ.Location = new System.Drawing.Point(10, 60);
+            this.uiWhere_Indexed_Dynamic_LINQ.Name = "uiWhere_Indexed_Dynamic_LINQ";
+            this.uiWhere_Indexed_Dynamic_LINQ.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_Indexed_Dynamic_LINQ.TabIndex = 4;
+            this.uiWhere_Indexed_Dynamic_LINQ.Text = "LINQ Dynamic";
+            this.uiWhere_Indexed_Dynamic_LINQ.UseVisualStyleBackColor = true;
+            this.uiWhere_Indexed_Dynamic_LINQ.Click += new System.EventHandler(this.uiWhere_Indexed_Dynamic_LINQ_Click);
+            // 
+            // uiWhere_Indexed_LINQ
+            // 
+            this.uiWhere_Indexed_LINQ.Location = new System.Drawing.Point(10, 20);
+            this.uiWhere_Indexed_LINQ.Name = "uiWhere_Indexed_LINQ";
+            this.uiWhere_Indexed_LINQ.Size = new System.Drawing.Size(100, 23);
+            this.uiWhere_Indexed_LINQ.TabIndex = 3;
+            this.uiWhere_Indexed_LINQ.Text = "LINQ";
+            this.uiWhere_Indexed_LINQ.UseVisualStyleBackColor = true;
+            this.uiWhere_Indexed_LINQ.Click += new System.EventHandler(this.uiWhere_Indexed_LINQ_Click);
+            // 
             // Where
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(771, 405);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -158,6 +302,9 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -174,5 +321,17 @@
         private System.Windows.Forms.Button uiWhere_Simple_2_Execute;
         private System.Windows.Forms.Button uiWhere_Simple_2_LINQ_Dynamic;
         private System.Windows.Forms.Button uiWhere_Simple_2_LINQ;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button uiWhere_Simple_3_Execute;
+        private System.Windows.Forms.Button uiWhere_Simple_3_LINQ_Dynamic;
+        private System.Windows.Forms.Button uiWhere_Simple_3_LINQ;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button uiWhere_DrillDown_LINQ;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Button uiWhere_Indexed_Execute_LINQ;
+        private System.Windows.Forms.Button uiWhere_Indexed_Dynamic_LINQ;
+        private System.Windows.Forms.Button uiWhere_Indexed_LINQ;
     }
 }
